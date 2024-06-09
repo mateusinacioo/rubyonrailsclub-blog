e48705ec0a1c8589e258116fbc8bcc3183d1bb91
mateusinacioo <mateus.inacio321@gmail.com>
2024-06-09 17:03:07 -0300

    separando area publica e administrativa do blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  namespace :administrate do
+    resources :articles
+  end
+
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -9,5 +15,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "welcome#index"
 end
