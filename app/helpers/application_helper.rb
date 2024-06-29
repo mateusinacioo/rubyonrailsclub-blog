932d982db7d10d14ada11d8d5448f2366841cd2e
mateusinacioo <mateus.inacio321@gmail.com>
2024-06-29 14:55:01 -0300

    criado controller publico para exibir artigo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-    def active_link?(controller_link)
-        "active" if controller_name == controller_link
-    end
+  def active_link?(controller_link)
+    "active" if controller_name == controller_link
+  end
 end
