874b85b6682ce4d8cd3d06e9411f6a5a1a3f050c
mateusinacioo <mateus.inacio321@gmail.com>
2024-06-30 19:06:06 -0300

    adiciona model e task para cadastrar autores

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.friendly.find(params[:id])
-        @other_articles = Article.all.sample(3)
-    end
+  def show
+    @article = Article.friendly.find(params[:id])
+    @other_articles = Article.all.sample(3)
+  end
 end
