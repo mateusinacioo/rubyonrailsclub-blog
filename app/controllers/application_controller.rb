f35dadac7e4e8b975cfb9f0caa47b736807d1dba
mateusinacioo <mateus.inacio321@gmail.com>
2024-07-03 16:04:37 -0300

    Finalizar ajustes no rodape

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :set_footer_date
+
+    def set_footer_date
+
+        return if params[:controller].split("/").first == "administrate"
+        
+        @footer_categories_1 = Category.all.order(created_at: :asc).limit(4)
+        @footer_categories_2 = Category.all.order(created_at: :desc).limit(4)
+        @footer_articles = Article.all.sample(4)
+    end
 end
