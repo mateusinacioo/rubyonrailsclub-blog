ef6a68b21dfe7969bcd7be742bddc066d6226b8a
mateusinacioo <mateus.inacio321@gmail.com>
2024-07-04 21:09:31 -0300

    Adicionado like e dislike nos comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,31 @@ class CommentsController < ApplicationController
 
     if comment.save
       redirect_to(article_path(@article), notice: "Comentário criado com sucesso!")
-    else 
-      redirect_to(article_path(@article), alert: "Erro ao criar comentário! - #{comment.errors.full_messages.join(",")}")
+    else
+      redirect_to(
+        article_path(@article),
+        alert: "Erro ao criar comentário! - #{comment.errors.full_messages.join(",")}",
+      )
+    end
+  end
+
+  def like
+    @article = Article.friendly.find(params[:article_id])
+
+    if Comment.find(params[:id]).increment!(:like)
+      redirect_to(article_path(@article), notice: "Like registrado!")
+    else
+      redirect_to(article_path(@article), notice: "Não foi possível registrar o like!")
+    end
+  end
+
+  def dislike
+    @article = Article.friendly.find(params[:article_id])
+
+    if Comment.find(params[:id]).increment!(:dislike)
+      redirect_to(article_path(@article), notice: "Dislike registrado!")
+    else
+      redirect_to(article_path(@article), notice: "Não foi possível registrar o Dislike!")
     end
   end
 
